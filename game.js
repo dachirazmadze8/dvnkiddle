5ff3eff5f148575d38a37f470dbb6596918c7ec3
dachirazmadzefrontend <dachi.razmadzete07@geolab.edu.ge>
2026-07-19 15:50:09 +0400

    fixed a bug with first encounter and added almost all enemies

--- game.js
+++ game.js
@@ -11,12 +11,59 @@ const enemyDatabase = {
     "gunner": { name: "Gunner", type: "Advanced", health: 350, waves: 22, encounter: "Wave 5 siege" },
     "sniper": { name: "Sniper", type: "Advanced", health: 100, waves: 23, encounter: "Wave 5 siege" },
     "engineer": { name: "Engineer", type: "Advanced", health: 150, waves: 14, encounter: "Wave 4 siege" },
+    "tranquailizer": { name: "Tranquailizer", type: "Advanced", health: 100, waves: 8, encounter: "Wave 2 epilogue" },
+    "medic": { name: "Medic", type: "Advanced", health: 200, waves: 10, encounter: "Wave 2 epilogue" },
     "level 1 sentry": { name: "Level 1 Sentry", type: "Mech", health: 300, waves: 14, encounter: "Wave 4 siege" },
     "level 2 sentry": { name: "Level 2 Sentry", type: "Mech", health: 450, waves: 14, encounter: "Wave 4 siege" },
     "level 3 sentry": { name: "Level 3 Sentry", type: "Mech", health: 600, waves: 14, encounter: "Wave 4 siege" },
     "level 1 teleporter": { name: "Level 1 teleporter", type: "Mech", health: 300, waves: 14, encounter: "Wave 4 siege" },
     "level 2 teleporter": { name: "Level 2 teleporter", type: "Mech", health: 450, waves: 14, encounter: "Wave 4 siege" },
     "level 3 teleporter": { name: "Level 3 teleporter", type: "Mech", health: 600, waves: 14, encounter: "Wave 4 siege" },
+    "ranger": { name: "Ranger", type: "Mech", health: 150, waves: 16, encounter: "Wave 3 siege" },
+    "APU": { name: "APU", type: "Mech", health: 900, waves: 22, encounter: "Wave 4 siege" },
+    "tank": { name: "Tank", type: "Mech", health: 3500, waves: 14, encounter: "Wave 7 siege" },
+    "fuel tank (tank)": { name: "Fuel Tank (Tank)", type: "Mech", health: 350, waves: 14, encounter: "Wave 7 siege" },
+    "platform": { name: "Platform", type: "Mech", health: 7000, waves: 14, encounter: "Wave 7 siege" },
+    "emplacement": { name: "Emplacement", type: "Mech", health: 350, waves: 14, encounter: "Wave 7 siege" },
+    "combatant": { name: "Combatant", type: "Elite Fodder", health: 100, waves: 10, encounter: "Wave 1 siege" },
+    "informant": { name: "Informant", type: "Elite Fodder", health: 150, waves: 6, encounter: "Wave 5 siege" },
+    "confidant": { name: "Confidant", type: "Elite Fodder", health: 200, waves: 3, encounter: "Wave 8 siege" },
+    "agitator": { name: "Agitator", type: "Elite Fodder", health: 300, waves: 6, encounter: "Wave 5 siege" },
+    "fuel tank (agitator)": { name: "Fuel Tank (Agitator)", type: "Elite Fodder", health: 200, waves: 6, encounter: "Wave 5 siege" },
+    "agreement": { name: "Agreement", type: "Elite Fodder", health: 150, waves: 5, encounter: "Wave 4 siege" },
+    "jagant": { name: "Jagant", type: "Elite Fodder", health: 200, waves: 3, encounter: "Wave 8 siege" },
+    "jagant bomb": { name: "Jagant Bomb", type: "Elite Fodder", health: 1000, waves: 3, encounter: "Wave 8 siege" },
+    "bombardier": { name: "Bombardier", type: "Elite Advanced", health: 300, waves: 4, encounter: "Wave 7 siege" },
+    "fusilier": { name: "Fusilier", type: "Boss", health: 400, waves: 14, encounter: "Wave 3 siege" },
+    "daedalus": { name: "Daedalus", type: "Boss", health: 500, waves: 14, encounter: "Wave 3 siege" },
+    "tempest": { name: "Tempest", type: "Boss", health: 600, waves: 14, encounter: "Wave 3 siege" },
+    "lelantos": { name: "Lelantos", type: "Boss", health: 380, waves: 14, encounter: "Wave 3 siege" },
+    "gaia": { name: "Gaia", type: "Boss", health: 775, waves: 12, encounter: "Wave 6 siege" },
+    "escort shielder": { name: "Escort Shielder", type: "Elite Fodder", health: 300, waves: 12, encounter: "Wave 6 siege" },
+    "shield": { name: "Shield", type: "Elite Fodder", health: 350, waves: 12, encounter: "Wave 6 siege" },
+    "escort gunner": { name: "Escort Gunner", type: "Elite Advanced", health: 500, waves: 12, encounter: "Wave 6 siege" },
+    "prometheus": { name: "Prometheus", type: "Boss", health: 750, waves: 12, encounter: "Wave 6 siege" },
+    "fuel tank (prometheus)": { name: "Fuel Tank (Prometheus)", type: "Boss", health: 200, waves: 12, encounter: "Wave 6 siege" },
+    "hermes": { name: "Hermes", type: "Boss", health: 525, waves: 12, encounter: "Wave 6 siege" },
+    "rocket pods": { name: "Rocket Pods", type: "Boss", health: 150, waves: 12, encounter: "Wave 6 siege" },
+    "sparta": { name: "Sparta", type: "Boss", health: 440, waves: 10, encounter: "Wave 9 siege" },
+    "trident": { name: "Trident", type: "Boss", health: 675, waves: 10, encounter: "Wave 9 siege" },
+    "achilles": { name: "Achilles", type: "Boss", health: 280, waves: 10, encounter: "Wave 9 siege" },
+    "drone": { name: "Drone", type: "Boss", health: 75, waves: 11, encounter: "Wave 9 siege" },
+    "dreadnought": { name: "Dreadnought", type: "Boss", health: 16000, waves: 1, encounter: "Wave 10 siege" },
+    "chassis": { name: "Chassis", type: "Boss", health: 1300, waves: 1, encounter: "Wave 10 mastermind" },
+    "mastermind": { name: "Mastermind", type: "Boss", health: 125, waves: 1, encounter: "Wave 10 mastermind" },
+    "ares": { name: "Ares", type: "Boss", health: 1000, waves: 2, encounter: "Wave 3 epilogue" },
+    "london prime": { name: "London Prime", type: "???", health: 3000, waves: 1, encounter: "Sandbox" },
+    "manglenether345": { name: "MangleNether345", type: "???", health: 250, waves: 1, encounter: "Sandbox" },
+    "proteus": { name: "Proteus", type: "???", health: 700, waves: 1, encounter: "Sandbox" },
+    "a-10": { name: "A-10", type: "???", health: 800, waves: 1, encounter: "Sandbox" },
+    "the wicked": { name: "The Wicked", type: "???", health: 1550, waves: 1, encounter: "Sandbox" },
+    "sparchilles": { name: "Sparchilles", type: "???", health: 440, waves: 1, encounter: "Sandbox" },
+    "mega joe": { name: "Mega Joe", type: "???", health: 3000, waves: 1, encounter: "Sandbox" },
+    "baby": { name: "Baby", type: "???", health: 200, waves: 1, encounter: "Sandbox" },
+    "jetnuker": { name: "JetNuker", type: "???", health: 100, waves: 1, encounter: "Sandbox" },
+    "zeus": { name: "Zeus", type: "???", health: 545, waves: 1, encounter: "Sandbox" },
 };
 
 // Chronological internal order of first encounters
@@ -142,8 +189,10 @@ function submitGuess() {
     function createEncounterCell(guessedEncounter, targetEncounter, threshold) {
         const td = document.createElement("td");
 
-        const guessedIndex = encounterOrder.indexOf(guessedEncounter);
-        const targetIndex = encounterOrder.indexOf(targetEncounter);
+        // Map the array to lower case to eliminate "Sandbox" vs "sandbox" issues
+        const lowerCaseOrder = encounterOrder.map(item => item.toLowerCase());
+        const guessedIndex = lowerCaseOrder.indexOf(guessedEncounter.toLowerCase());
+        const targetIndex = lowerCaseOrder.indexOf(targetEncounter.toLowerCase());
 
         if (guessedIndex === targetIndex) {
             td.innerText = guessedEncounter;
@@ -154,7 +203,7 @@ function submitGuess() {
 
             // Check if within index position threshold steps
             const difference = Math.abs(guessedIndex - targetIndex);
-            if (difference <= threshold) {
+            if (guessedIndex !== -1 && targetIndex !== -1 && difference <= threshold) {
                 td.classList.add("cell-partial"); // Yellow box indicator
             } else {
                 td.classList.add("cell-incorrect"); // Red box indicator
